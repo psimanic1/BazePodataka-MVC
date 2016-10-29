a9fa4055c1f0823eaf47f553c8ae62e8018bd0da
psimanic1 <psimanic1@etf.unsa.ba>
2016-10-29 19:09:17 +0200

    BP-Projekat

---
+++ MVC-BP/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_BP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PonudaVozila()
+        {
+            ViewBag.Message = "Naša ponuda vozila.";
+
+            return View();
+        }
+
+    }
+}
